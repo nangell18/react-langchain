26179dd8ff7027e870fc4665ee002c11de40cce8
nangell18 <nate@om-bi.com>
2025-06-10 21:10:32 -0400

    implemented first tool inside my program
    this is first draft of the main.py file and I can call a simple function and it returns the length of any character I input. notice the description I have right below the name of the function

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from dotenv import load_dotenv
+load_dotenv()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+def get_text_length(text:str) -> int:
+    """Returns the length of a text by characters""" # this description is important because this is going to decide if the LLM is going to use this tool or not in its reasoning engine
+    return len(text)
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("Hello ReAct LangChain!")
+    print(get_text_length(text="Nate"))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
